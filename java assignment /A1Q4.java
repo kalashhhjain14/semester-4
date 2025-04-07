b34322e6fc2afd4b4607034b0fa67867de20ccd2
kalash jain <kalashjain861@gmail.com>
2025-04-07 17:25:27 +0530

    Create A1Q4.java

---
+++ java assignment /A1Q4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class A1Q4
+{
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int num=s.nextInt();
+        System.out.println("Square of  the Number : "+num*num);
+        System.out.println("Cube of the Number : " +num*num*num);    
+    }
+}
