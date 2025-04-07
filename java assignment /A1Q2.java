277c6fec5615b96148180ff44bedffe3ff57e5e2
kalash jain <kalashjain861@gmail.com>
2025-04-07 17:22:50 +0530

    Create A1Q2.java

---
+++ java assignment /A1Q2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class assignment2
+{
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in) ;
+        System.out.println("Enter the Farenheit = ");
+        int faren=s.nextInt();
+        int cel=(faren-32)*5/9;
+        System.out.println("Celcius = "+cel);
+    }
+}
