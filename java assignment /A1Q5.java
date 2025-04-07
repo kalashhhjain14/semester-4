80ff95857bc978d1bb0d878915587f5c92ab473e
kalash jain <kalashjain861@gmail.com>
2025-04-07 17:26:14 +0530

    Create A1Q5.java

---
+++ java assignment /A1Q5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class A1Q5
+{
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the Radius : ");
+        int rad=s.nextInt();
+        float arcirc=3.1416f*rad*rad;
+        System.out.println("Area of Circle = "+arcirc); 
+    }
+}
